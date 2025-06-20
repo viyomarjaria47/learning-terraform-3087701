bb6fe0237f78283a1822b65c832dbed964e930d7
viyomarjaria47 <viyomarjaria@yahoo.com>
2025-06-20 12:09:19 +0100

    correct alb name

--- main.tf
+++ main.tf
@@ -40,10 +40,10 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "blog_alb" {
+module "blog-alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  name    = "blog_alb"
+  name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
