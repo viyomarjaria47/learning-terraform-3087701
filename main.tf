f455a0531227788b0ffd1fa968ff1a42705d7628
viyomarjaria47 <viyomarjaria@yahoo.com>
2025-06-20 12:00:45 +0100

    changed intance id

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      target_id        = aws_instance.instance_id
+      target_id        = aws_instance.blog.instance_id
   }
 
   tags = {
